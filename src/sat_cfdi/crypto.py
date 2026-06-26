1713726bac3e71ea7d0ca5ccad3a53250792db03
Christian ZH <christianzh260@gmail.com>
2026-06-26 16:37:11 -0600

    refactor: use pathlib for project paths

--- src/sat_cfdi/crypto.py
+++ src/sat_cfdi/crypto.py
@@ -2,10 +2,12 @@ from pathlib import Path
 from cryptography import x509
 
 
-def load_certificate(path: str):
+def load_certificate(path: str | Path) -> x509.Certificate:
     """
     Load a DER encoded X509 certificate.
     """
+
+    path = Path(path)
 
     with open(path, "rb") as cert_file:
         return x509.load_der_x509_certificate(cert_file.read())
